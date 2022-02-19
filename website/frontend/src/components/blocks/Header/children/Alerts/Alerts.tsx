5ee55aef2f8aecd3e6f3ff54877361cc8c7ac560
Nikita Yutanov <nikitayutanov@gmail.com>
2022-02-19 14:39:48 +0300

    Drop notifications page leftovers

--- website/frontend/src/components/blocks/Header/children/Alerts/Alerts.tsx
+++ website/frontend/src/components/blocks/Header/children/Alerts/Alerts.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
-import { routes } from 'routes';
-import { RootState } from 'store/reducers';
 import Bell from 'assets/images/bell.svg';
 import { Counter } from './Counter/Counter';
 import { Dropdown } from './Dropdown/Dropdown';
 import styles from './Alerts.module.scss';
 
 const Alerts = () => {
-  // TOFIX: countUnread type to be number not number | null;
-  const { countUnread } = useSelector((state: RootState) => state.notifications);
-  const unreadAmount = Number(countUnread);
+  const unreadAmount = 2;
   const isAnyAlert = unreadAmount > 0;
   const className = clsx('img-wrapper', styles.button);
 
   return (
     <div className={styles.alerts}>
-      <Link to={routes.notifications} className={className}>
+      <Link to={`#`} className={className}>
         {isAnyAlert && <Counter value={unreadAmount} />}
         <img src={Bell} alt="alerts" />
       </Link>
