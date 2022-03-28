a8d5b15ca5f860e7a3d9008a7f04845bac66f0b9
Nikita Yutanov <nikitayutanov@gmail.com>
2022-03-29 00:23:11 +0300

    Add submit

--- website/frontend/src/components/pages/Form/Form.tsx
+++ website/frontend/src/components/pages/Form/Form.tsx
@@ -1,24 +1,29 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { createPayloadTypeStructure, decodeHexTypes, Metadata } from '@gear-js/api';
-import { Input } from '@gear-js/ui';
-import { Payload } from './children';
-import styles from './Form.module.scss';
+import { createPayloadTypeStructure, decodeHexTypes, GearKeyring, Hex, Metadata } from '@gear-js/api';
+import { Button, Input } from '@gear-js/ui';
+import { GetMetaResponse } from 'api/responses';
 import { getMeta } from 'services';
 import { getPreformattedText } from 'helpers';
-import { GetMetaResponse } from 'api/responses';
+import { useAccount, useApi } from 'hooks';
+import { Payload } from './children';
+import styles from './Form.module.scss';
+import { web3FromSource } from '@polkadot/extension-dapp';
 
-type Params = { destination: string };
+type Params = { destination: Hex };
 type MetadataResponse = { result: GetMetaResponse | undefined };
 
-const initValues = { payload: '', gasLimit: '20000000', value: '0' };
+const initValues = { payload: '{ "RequestForMembership": "123" }', gasLimit: '20000000', value: '0' };
 
 const Form = () => {
+  const { api } = useApi();
+  const { account } = useAccount();
   const { destination } = useParams() as Params;
 
+  const [meta, setMeta] = useState<Metadata>();
   const [values, setValues] = useState(initValues);
 
-  const getMetadata = ({ result }: MetadataResponse) =>
+  const getParsedMeta = ({ result }: MetadataResponse) =>
     result ? (JSON.parse(result.meta) as Metadata) : Promise.reject('No metadata');
 
   const getTypeStructure = ({ types, handle_input: handleInput }: Metadata) =>
@@ -26,16 +31,26 @@ const Form = () => {
       ? createPayloadTypeStructure(handleInput, decodeHexTypes(types), true)
       : Promise.reject("Can't decode");
 
-  const updatePayload = (payloadValue: string) => {
-    setValues((prevValues) => ({ ...prevValues, payload: payloadValue }));
+  const updateMeta = (parsedMeta: Metadata) => {
+    setMeta(parsedMeta);
+    return parsedMeta;
+  };
+
+  const updatePayload = (payload: string) => {
+    setValues((prevValues) => ({ ...prevValues, payload }));
+  };
+
+  const updateGasLimit = (gasLimit: string) => {
+    setValues((prevValues) => ({ ...prevValues, gasLimit }));
   };
 
   useEffect(() => {
     getMeta(destination)
-      .then(getMetadata)
+      .then(getParsedMeta)
+      .then(updateMeta)
       .then(getTypeStructure)
       .then(getPreformattedText)
-      .then(updatePayload)
+      // .then(updatePayload)
       .catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -44,8 +59,37 @@ const Form = () => {
     setValues((prevValues) => ({ ...prevValues, [name]: value }));
   };
 
+  const calculateGas = () => {
+    if (account && meta) {
+      const { address } = account;
+      const { payload, value } = values;
+      const publicKey = GearKeyring.decodeAddress(address);
+
+      api.program.gasSpent
+        .handle(publicKey, destination, payload, value, meta)
+        .then((gas) => gas.toString())
+        .then(updateGasLimit);
+    }
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (account) {
+      const { address } = account;
+      const { source } = account.meta;
+      const message = { destination, ...values };
+
+      api.message.submit(message, meta);
+
+      web3FromSource(source)
+        .then(({ signer }) => ({ signer }))
+        .then((options) => api.message.signAndSend(address, options));
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Input label="Destination:" className={styles.input} value={destination} readOnly />
       <Payload name="payload" value={values.payload} onChange={handleChange} />
       <Input
@@ -56,6 +100,11 @@ const Form = () => {
         onChange={handleChange}
       />
       <Input label="Value:" className={styles.input} name="value" value={values.value} onChange={handleChange} />
+      <div className={styles.buttons}>
+        {/* omit type='button' after gear-ui update */}
+        <Button text="Calculate gas" color="secondary" type="button" onClick={calculateGas} />
+        <Button text="Send message" type="submit" />
+      </div>
     </form>
   );
 };
